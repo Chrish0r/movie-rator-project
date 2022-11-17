e4e5c149802f7c22d89bb9127d36edf0a59e06f1
Chrish0r <Christof.Gadek10@gmail.com>
2022-11-17 20:58:08 +0100

    refactored project - removed duplicated packages

--- src/main/java/de/othr/im/model/MediaEntity.java
+++
@@ -1,8 +0,0 @@
-package de.othr.im.model;
-
-import javax.persistence.MappedSuperclass;
-
-@MappedSuperclass
-public abstract class MediaEntity {
-
-}
--- src/main/java/de/othr/im/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package de.othr.im.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import de.othr.im.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
