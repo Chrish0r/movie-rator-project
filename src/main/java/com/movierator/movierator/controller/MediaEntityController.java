6eb4f7f977c86226d0df46ce012907341b6665bb
Jonas Meier <jonas.meier1212@gmail.com>
2022-12-30 21:42:41 +0100

    Added media detail page

--- src/main/java/com/movierator/movierator/controller/MediaEntityController.java
+++ src/main/java/com/movierator/movierator/controller/MediaEntityController.java
@@ -2,16 +2,22 @@ package com.movierator.movierator.controller;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.movierator.movierator.controller.formObjects.SearchResult;
 import com.movierator.movierator.controller.formObjects.SearchTerm;
 import com.movierator.movierator.model.MediaEntity;
 import com.movierator.movierator.repository.MediaEntityRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class MediaEntityController {
@@ -26,14 +32,26 @@ public class MediaEntityController {
     model.addAttribute("searchTerm", searchTerm);
     
     List<MediaEntity> foundMedia = this.mediaEntityRepository.findByNameContaining(searchTerm.getValue());
-    // TODO
     List<SearchResult> results = new ArrayList<>(foundMedia.size());
     
     for (MediaEntity media : foundMedia) {
-      results.add(new SearchResult(media.getName()));
+      results.add(new SearchResult(media.getId(), media.getName()));
     }
     model.addAttribute("results", results);
 
     return "search-result";
   }
+
+  @GetMapping(value="/media/{id}")
+  public String showMedia(@PathVariable long id, Model model) {
+    Optional<MediaEntity> media = this.mediaEntityRepository.findById(id);
+    
+    if(media.isEmpty()) {
+      return "redirect:not-found";
+    }
+    
+    model.addAttribute("media", media.get());
+    return "media-detail";
+  }
+  
 }
--- src/main/java/com/movierator/movierator/controller/formObjects/SearchResult.java
+++ src/main/java/com/movierator/movierator/controller/formObjects/SearchResult.java
@@ -2,16 +2,26 @@ package com.movierator.movierator.controller.formObjects;
 
 public class SearchResult {
   private String title;
+  private long id;
 
-  public SearchResult(String title) {
+  public SearchResult(long id, String title) {
+    this.id = id;
     this.title = title;
   }
+
+  public long getId() {
+    return id;
+  }
+
+  // public void setId(long id) {
+  //   this.id = id;
+  // }
 
   public String getTitle() {
     return title;
   }
 
-  public void setTitle(String title) {
-    this.title = title;
-  }
+  // public void setTitle(String title) {
+  //   this.title = title;
+  // }
 }
