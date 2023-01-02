adc609e24db6411b198190f6b74ef5b945b63887
Jonas Meier <jonas@b-42.com>
2023-01-02 22:15:23 +0100

    Added Docker Compose for easier local development

--- start-db.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --name sw-project-movie-rator -e MYSQL_ROOT_PASSWORD=password -p 3306:3306 -d mysql:latest
--- stop-db.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-docker stop sw-project-movie-rator
-docker rm sw-project-movie-rator
