d2417d3232aa08bf765abf514aa7a416754f3fe1
Jonas Meier <jonas@b-42.com>
2023-01-03 21:48:59 +0100

    Use fallback `DummyMailSender` if properties aren't set

--- src/main/java/com/movierator/movierator/service/NewsletterSender.java
+++ src/main/java/com/movierator/movierator/service/NewsletterSender.java
@@ -3,13 +3,11 @@ package com.movierator.movierator.service;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.mail.search.ReceivedDateTerm;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailSender;
 import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
@@ -19,14 +17,15 @@ import com.movierator.movierator.repository.NewsletterSubscriberRepository;
 @Service
 public class NewsletterSender {
   private final Logger logger = LoggerFactory.getLogger(NewsletterSender.class);
-  
+
   @Autowired
   private NewsletterSubscriberRepository newsletterSubscriberRepository;
 
-//  @Autowired
-//  private JavaMailSender emailSender;
+  @Autowired
+  private MailSender emailSender;
 
-  @Scheduled(fixedRate = 60000) // TODO: Add Cron expression to send newsletter weekly
+  @Scheduled(fixedRate = 60000) // TODO: Add Cron expression to send newsletter
+  // weekly
   public void sendNewsletter() {
     logger.info("Sending newsletter...");
     Iterable<NewsletterSubscriber> subscribers = newsletterSubscriberRepository.findAll();
@@ -35,14 +34,14 @@ public class NewsletterSender {
     for (NewsletterSubscriber newsletterSubscriber : subscribers) {
       recipients.add(newsletterSubscriber.getEmail());
     }
-    if(recipients.isEmpty()) {
+    if (recipients.isEmpty()) {
       logger.info("No newsletter subscribers. Skip sending.");
       return;
     }
-    
+
     SimpleMailMessage message = getNewsletterMessage();
     message.setTo((recipients.toArray(new String[0])));
-//    emailSender.send(message);
+    emailSender.send(message);
 
     logger.info(String.format("Sent newsletter to %d subscribers", recipients.size()));
   }
