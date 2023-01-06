b473c7e15520cbf3422b887bb4774b6840dd957c
Chrish0r <Christof.Gadek10@gmail.com>
2023-01-06 16:06:54 +0100

    removed LoginController.java.orig-file

--- src/main/java/com/movierator/movierator/config/SecurityConfiguration.java
+++ src/main/java/com/movierator/movierator/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,7 +21,7 @@ public class SecurityConfiguration {
 
 	@Autowired
 	MyUserDetailsService userDetailsService;
-
+		    
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
