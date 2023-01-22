77ff560d44ec8facb7d05e476472580d35c920e6
Chrish0r <Christof.Gadek10@gmail.com>
2023-01-22 15:42:30 +0100

    removed unnecessary classes (email service) and refactoring

--- src/main/java/com/movierator/movierator/MovieratorApplication.java
+++ src/main/java/com/movierator/movierator/MovieratorApplication.java
@@ -1,16 +1,10 @@
 package com.movierator.movierator;
 
 import java.util.Locale;
-import java.util.Properties;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.EventListener;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -22,36 +16,10 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 @SpringBootApplication
 @EnableScheduling
 public class MovieratorApplication implements WebMvcConfigurer {
-	
-//	 @Autowired
-//	 private EmailService emailService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieratorApplication.class, args);
 	}
-	
-//	@Bean
-//	public JavaMailSender getJavaMailSender() {
-//	    JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-//	    mailSender.setHost("smtp.gmail.com");
-//	    mailSender.setPort(587);
-//	      
-//	    mailSender.setUsername("anna.updated@gmail.com");
-//	    mailSender.setPassword("yafcviyjmpszudfo");
-//	      
-//	    Properties props = mailSender.getJavaMailProperties();
-//	    props.put("mail.transport.protocol", "smtp");
-//	    props.put("mail.smtp.auth", "true");
-//	    props.put("mail.smtp.starttls.enable", "true");
-//	    props.put("mail.debug", "true");
-//	      
-//	    return mailSender;
-//	}
-	
-//	@EventListener(ApplicationReadyEvent.class)
-//	public void sendEmail() {
-//		this.emailService.sendEmail("Haxton.endgame@gmail.com", "this is the subject", "This is the body");
-//	}
 	
 	@Bean
 	public LocaleResolver localeResolver() {
