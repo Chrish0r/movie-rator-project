85caaf619f773eefc37b76374914c20804f00aa2
Chrish0r <Christof.Gadek10@gmail.com>
2023-01-24 16:56:30 +0100

    removed unnecessary method and view

--- src/main/java/com/movierator/movierator/controller/LoginController.java
+++ src/main/java/com/movierator/movierator/controller/LoginController.java
@@ -1,13 +1,8 @@
 package com.movierator.movierator.controller;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
@@ -16,11 +11,5 @@ public class LoginController {
 	public String showLoginForm(Model model) {
 
 		return "login";
-	}
-
-	@RequestMapping(method = RequestMethod.GET, value = "/prelogout")
-	public String showPreLogout(HttpServletRequest request, HttpServletResponse response) {
-
-		return "prelogout";
 	}
 }
